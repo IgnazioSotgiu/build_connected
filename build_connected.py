766af03f7a9495969ea5dc95be1f9b0d6d83e700
IgnazioSotgiu <ignazio_sotgiu@yahoo.it>
2021-05-19 22:13:15 +0000

    add POST method to register function

--- build_connected.py
+++ build_connected.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 import webbrowser
@@ -22,12 +23,42 @@ def welcome_page():
     return render_template("welcome-page.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        check_username = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        check_password = request.form.get("password")
+        check_confirm_password = request.form.get("confirm_password")
+
+        if check_username:
+            flash("Username already exists")
+            return redirect(url_for('register'))
+
+        elif check_password != check_confirm_password:
+            flash("Passwords don't match.")
+            flash("Please re-enter passwords")
+            return redirect(url_for('register'))
+
+        new_user = {
+            "username": request.form.get("username").lower(),
+            "company_name": request.form.get("company_name").lower(),
+            "contractor_type": request.form.get("contractor_type").lower(),
+            "categories": request.form.get("category").lower(),
+            "county": request.form.get("county").lower(),
+            "country": request.form.get("country").lower(),
+            "email": request.form.get("email"),
+            "phone_number": request.form.get("phone"),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(new_user)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Welcome {{'username'}}. Registration Succesful")
     return render_template("register.html")
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=os.environ.get("DEBUG"))
